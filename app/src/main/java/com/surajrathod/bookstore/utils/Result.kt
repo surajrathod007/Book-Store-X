125bb94626a8d5b988aa0d3439b69d9601392daa
SurajSinh Rathod <surajsinhrathod75@gmail.com>
2023-07-14 11:35:49 +0530

    Added Result.kt class for response handling

---
+++ app/src/main/java/com/surajrathod/bookstore/utils/Result.kt
@@ -0,0 +1,9 @@
+package com.surajrathod.bookstore.utils
+
+import java.lang.Exception
+
+sealed class Result<T>(val data : T?=null,val msg : String?=null){
+    class Success<T>(data : T,msg : String?=null) : Result<T>(data,msg)
+    class Loading<T>() : Result<T>()
+    class Failure<T>(msg: String?=null,exception: Exception?=null) : Result<T>(msg = msg)
+}
